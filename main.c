42107bcaee79356587f5d4cc25518ffedee18244
Bailey <bfox200012@gmail.com>
2016-06-30 11:29:14 -0500

    Add code

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <ctr/console.h>
+#include <ctr/printf.h>
+#include <ctr/draw.h>
+#include "sdmmc/sdmmc.h"
+
+#define PROD 0
+
+void scribble_screen(void);
+
+int main(void){
+	uint8_t CID[16];
+	
+	draw_init((draw_s*) 0x23FFFE00);
+	console_init(0xFFFFFF, 0);
+	draw_clear_screen(SCREEN_TOP, 0x1111111);
+	
+	printf("Getting CID..\n\n");
+	sdmmc_get_cid(PROD, (uint32_t*) CID);
+	for(int i = 0; i < sizeof(CID); i++){
+		printf("%02X", CID[i]);
+	}
+}
